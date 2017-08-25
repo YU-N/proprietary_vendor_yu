e0622814a725d6139026d03e50d130031ec390a1
DROHAN898 <drohan940@gmail.com>
2017-08-25 08:50:52 +0000

    lettuce: Use BUILD_PREBUILT for QtiTelephonyService
    Signed-off-by: DROHAN898 <drohan940@gmail.com>

--- lettuce/Android.mk
+++ lettuce/Android.mk
@@ -86,6 +86,17 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE := QtiTelephonyService
+LOCAL_MODULE_OWNER := yu
+LOCAL_SRC_FILES := proprietary/app/QtiTelephonyService/QtiTelephonyService.apk
+LOCAL_CERTIFICATE := platform
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := APPS
+LOCAL_DEX_PREOPT := false
+LOCAL_MODULE_SUFFIX := .apk
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE := fastdormancy
 LOCAL_MODULE_OWNER := yu
 LOCAL_SRC_FILES := proprietary/app/fastdormancy/fastdormancy.apk
